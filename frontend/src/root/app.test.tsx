289280bc1763b67caf849ce94a7a1a6e445681aa
Ben Honeywill <benhoneywill@gmail.com>
2022-01-19 16:15:38 +0000

    fix test

--- frontend/src/root/app.test.tsx
+++ frontend/src/root/app.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./app";
 
 test("The app renders", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const appElement = screen.getByTestId("app");
   expect(appElement).toBeInTheDocument();
 });
