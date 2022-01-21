5b674d00b21632f966d8ef90b1bbd1c05478bd85
Jake Gealer <jake.gealer@krystal.uk>
2022-01-21 16:53:14 +0000

    add trace route json/text api

--- backend/api_v1/traceroute.go
+++ backend/api_v1/traceroute.go
@@ -1,7 +1,10 @@
 package api_v1
 
 import (
+	"fmt"
 	"net"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -10,14 +13,35 @@ import (
 )
 
 type tracerouteParams struct {
+	// Timeout is used to define how long to wait for a response from the remote host.
 	Timeout uint `form:"timeout"`
-	TTL     uint `form:"ttl"`
+
+	// Hop is used to define which hop should be ran.
+	Hop uint `form:"hop"`
+
+	// TotalHops is an alternative to the hop parameter which defines the total number of hops that should be ran.
+	TotalHops uint `form:"total_hops"`
+}
+
+// TraceResponse is used to define a response within the traceroute slice.
+type TraceResponse struct {
+	// Pings is used to define the latency of all pings sent.
+	Pings [3]*float64 `json:"pings"`
+
+	// RDNS is used to define the RDNS of the host if valid.
+	RDNS *string `json:"rdns"`
+
+	// IPAddress is used to define the IP address of the host.
+	IPAddress string `json:"ip_address"`
 }
 
 func traceroute(g *gin.RouterGroup, log *zap.Logger) {
 	g.GET("/:hostnameOrIp", func(ctx *gin.Context) {
 		// Get the hostname or IP.
 		hostnameOrIp := ctx.Param("hostnameOrIp")
+
+		// Defines if this is JSON.
+		isJson := ctx.ContentType() == "application/json"
 
 		// Parse the traceroute parameters.
 		var p tracerouteParams
@@ -26,52 +50,151 @@ func traceroute(g *gin.RouterGroup, log *zap.Logger) {
 			return
 		}
 
+		// Defines all hops that need to be ran.
+		if p.TotalHops > 64 || (p.Hop == 0 && p.TotalHops == 0) {
+			p.TotalHops = 64
+		}
+		hops := []uint{p.Hop}
+		if p.TotalHops != 0 {
+			hops = make([]uint, p.TotalHops)
+			for i := uint(0); i < p.TotalHops; i++ {
+				hops[i] = i + 1
+			}
+		}
+
 		// Get the addresses.
 		addrs, err := net.LookupHost(hostnameOrIp)
 		if err != nil {
-			ctx.String(400, "invalid hostname or IP")
+			if isJson {
+				ctx.JSON(400, map[string]string{
+					"message": "invalid hostname or IP",
+				})
+			} else {
+				ctx.String(400, "invalid hostname or IP")
+			}
 			return
 		}
 		ipAddr := net.ParseIP(addrs[0])
 
-		// Build the tracer.
-		t := &traceroutego.Tracer{
-			Config: traceroutego.Config{
-				Timeout:  time.Millisecond * 100,
-				Delay:    time.Duration(1),
-				MaxHops:  1,
-				Networks: []string{"ip4:icmp", "ip4:ip", "ip6:icmp", "ip6:ip"},
-			},
-		}
-		defer t.Close()
-
-		// Make the session.
-		s, err := t.NewSession(ipAddr)
-		if err != nil {
-			ctx.String(500, "Internal Server Error")
-			log.Error("failed to start traceroute session", zap.Error(err))
-			return
-		}
+		// Set the default timeout.
 		if p.Timeout == 0 || p.Timeout > 1000 {
 			p.Timeout = 1000
 		}
-		ttl := int(p.TTL)
-		if ttl == 0 {
-			ttl = 64
-		}
-		if err = s.Ping(ttl); err != nil {
-			ctx.String(500, "Internal Server Error")
-			log.Error("failed to ping host", zap.Error(err))
-			return
+
+		// Go through each hop.
+		strResponses := []string{}
+		jsonResponses := []*TraceResponse{}
+		var s *traceroutego.Session
+	hopFor:
+		for _, hop := range hops {
+			breakState := (func() int {
+				// Get all needed replies.
+				replies := [3]*traceroutego.Reply{}
+				pings := [3]*float64{}
+				for i := 0; i < 3; i++ {
+					// Build the tracer.
+					t := &traceroutego.Tracer{
+						Config: traceroutego.Config{
+							Timeout:  time.Millisecond * 100,
+							Delay:    time.Duration(1),
+							MaxHops:  1,
+							Networks: []string{"ip4:icmp", "ip4:ip", "ip6:icmp", "ip6:ip"},
+						},
+					}
+					defer t.Close()
+
+					// Make the session.
+					s, err = t.NewSession(ipAddr)
+					if err != nil {
+						ctx.String(500, "Internal Server Error")
+						log.Error("failed to start traceroute session", zap.Error(err))
+						return 0
+					}
+					ttl := int(hop)
+					if ttl == 0 {
+						ttl = 1
+					}
+					startTime := time.Now()
+					if err = s.Ping(ttl); err != nil {
+						ctx.String(500, "Internal Server Error")
+						log.Error("failed to ping host", zap.Error(err))
+						return 0
+					}
+
+					// Handle the timeout.
+					select {
+					case r := <-s.Receive():
+						replies[i] = r
+						p := float64(time.Now().Sub(startTime).Nanoseconds() / 1000000)
+						pings[i] = &p
+					case <-time.After(time.Millisecond * time.Duration(p.Timeout)):
+						// Just a timeout. Do nothing.
+					}
+				}
+
+				// Get the RDNS.
+				var rdns *string
+				if replies[0] != nil {
+					// Get the replying IP address.
+					replyingIp := replies[0].IP
+
+					// Get the RDNS.
+					if hosts, _ := net.LookupAddr(replyingIp.String()); hosts != nil && len(hosts) > 0 {
+						rdns = &hosts[0]
+					}
+				}
+
+				// Handle string or JSON formatting.
+				if isJson {
+					// If this is nil, that's okay, that is how we repersent a timeout.
+					if replies[0] != nil {
+						jsonResponses = append(jsonResponses, &TraceResponse{
+							Pings:     pings,
+							RDNS:      rdns,
+							IPAddress: replies[0].IP.String(),
+						})
+					}
+				} else {
+					if replies[0] == nil {
+						strResponses = append(strResponses, strconv.FormatUint(uint64(hop), 10)+"\t*\t*\t*\t*\t")
+					} else {
+						resp := replies[0].IP.String()
+						if rdns != nil {
+							resp += " (" + *rdns + ")"
+						}
+						resp += "\t"
+						for _, pi := range pings {
+							if pi == nil {
+								resp += "*\t"
+							} else {
+								resp += fmt.Sprint(*pi) + "\t"
+							}
+						}
+						strResponses = append(strResponses, resp)
+					}
+				}
+
+				// Break the hop for loop if the IP is the same.
+				if replies[0] != nil && ipAddr.Equal(replies[0].IP) {
+					return 1
+				}
+				return 2
+			})()
+
+			// Decide how to handle this hop.
+			switch breakState {
+			case 0:
+				return
+			case 1:
+				break hopFor
+			}
 		}
 
-		// Handle the timeout.
-		// TODO: format this better, handle multi hop formatting
-		select {
-		case r := <-s.Receive():
-			ctx.String(200, r.IP.String())
-		case <-time.After(time.Millisecond * time.Duration(p.Timeout)):
-			ctx.String(400, "trace timeout")
+		// Return either JSON or string responses.
+		if isJson {
+			ctx.JSON(200, jsonResponses)
+		} else {
+			ctx.String(200, strings.Join(strResponses, "\n"))
 		}
 	})
 }
