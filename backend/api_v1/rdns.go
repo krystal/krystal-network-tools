b3b2dddc93dc2460911dec0c23f47e053406cb77
Jake Gealer <jake.gealer@krystal.uk>
2022-01-31 12:37:31 +0000

    rdns switcheroo

--- backend/api_v1/rdns.go
+++ backend/api_v1/rdns.go
@@ -9,22 +9,22 @@ import (
 func rdns(g *gin.RouterGroup) {
 	g.GET("/:ip", func(ctx *gin.Context) {
 		ip := ctx.Param("ip")
-		if hosts, _ := net.LookupAddr(ip); hosts != nil && len(hosts) > 0 {
+		hosts, err := net.LookupAddr(ip)
+		if err != nil || len(hosts) == 0 {
 			if ctx.ContentType() == "application/json" {
-				ctx.JSON(200, map[string]string{
-					"hostname": hosts[0],
+				ctx.JSON(400, map[string]string{
+					"message": "Failed to find IP",
 				})
 			} else {
-				ctx.String(200, hosts[0])
+				ctx.String(400, "Failed to find IP")
 			}
-			return
 		}
 		if ctx.ContentType() == "application/json" {
-			ctx.JSON(400, map[string]string{
-				"message": "Failed to find IP",
+			ctx.JSON(200, map[string]string{
+				"hostname": hosts[0],
 			})
 		} else {
-			ctx.String(400, "Failed to find IP")
+			ctx.String(200, hosts[0])
 		}
 	})
 }
