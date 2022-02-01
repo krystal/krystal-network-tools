8e30a0f9e445138f9cec4e112893f347bc1ea0c8
Jake Gealer <jake.gealer@krystal.uk>
2022-02-01 10:41:26 +0000

    also comment out ping ratelimit

--- backend/api_v1/init.go
+++ backend/api_v1/init.go
@@ -1,21 +1,18 @@
 package api_v1
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
-	"github.com/krystal/krystal-network-tools/backend/ratelimiter"
 	"go.uber.org/zap"
 )
 
 // Init initializes the API.
 func Init(g *gin.RouterGroup, log *zap.Logger, cachedDnsServer string) {
 	// Create the base bucket for a few types of requests.
-	pingingBucket := ratelimiter.NewBucket(log, 75, time.Minute, time.Minute*10)
+	//pingingBucket := ratelimiter.NewBucket(log, 75, time.Minute, time.Minute*10)
 
 	// Load the routes.
 	userIp(g)
-	ping(g.Group("/ping", pingingBucket))
+	ping(g.Group("/ping")) //, pingingBucket))
 	dns(
 		//g.Group("/dns", ratelimiter.NewBucket(log, 20, time.Hour, time.Minute*10))
 		g.Group("/dns"), log,
