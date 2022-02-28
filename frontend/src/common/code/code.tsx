1b12e465134ce6ae322b88400a49105d18958a98
Ben Honeywill <benhoneywill@gmail.com>
2022-02-28 15:47:11 +0000

    style the copy to clipboard function for code blocks

--- frontend/src/common/code/code.tsx
+++ frontend/src/common/code/code.tsx
@@ -1,34 +1,60 @@
-import { Box, BoxProps, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  BoxProps,
+  useColorModeValue,
+  Button,
+  HStack,
+  useToast,
+} from "@chakra-ui/react";
 import { FC } from "react";
 import { FaClipboard } from "react-icons/fa";
 
 const Code: FC<BoxProps> = ({ children, ...props }) => {
+  const toast = useToast();
+
   const copyToClipboard = () => {
-    const c = children?.toString();
-    if (c) navigator.clipboard.writeText(c);
+    const content = children?.toString();
+    if (content) {
+      navigator.clipboard.writeText(content);
+      toast({
+        status: "success",
+        title: "Copied to your clipboard succesfully",
+      });
+    }
   };
 
-  return <>
-    <Box
-      as="pre"
-      display="inline-block"
-      overflowX="auto"
-      bg={useColorModeValue("gray.200", "gray.900")}
-      color={useColorModeValue("gray.900", "gray.50")}
-      py={2}
-      px={4}
-      fontSize="xs"
-      fontFamily="monospace"
-      borderRadius="md"
-      maxW="100%"
-      {...props}
-    >
-      <code>{children}</code>
+  return (
+    <Box>
+      <HStack
+        p={2}
+        border="1px solid"
+        borderColor={useColorModeValue("gray.200", "gray.800")}
+        bg={useColorModeValue("white", "gray.700")}
+        borderTopRadius="md"
+        justify="flex-end"
+      >
+        <Button size="xs" onClick={copyToClipboard} leftIcon={<FaClipboard />}>
+          Copy to clipboard
+        </Button>
+      </HStack>
+
+      <Box
+        as="pre"
+        display="block"
+        overflowX="auto"
+        bg="gray.900"
+        color="gray.50"
+        p={4}
+        fontSize="xs"
+        fontFamily="monospace"
+        borderBottomRadius="md"
+        maxW="100%"
+        {...props}
+      >
+        <code>{children}</code>
+      </Box>
     </Box>
-    <button onClick={copyToClipboard}>
-      <FaClipboard></FaClipboard>
-    </button>
-  </>;
+  );
 };
 
 export default Code;
