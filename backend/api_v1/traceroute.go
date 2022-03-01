6f1581cbd8d75d89b60593b508b80dcf061ce89c
Jake Gealer <jake.gealer@krystal.uk>
2022-03-01 09:52:32 +0000

    bugfix: increase timeouts

--- backend/api_v1/traceroute.go
+++ backend/api_v1/traceroute.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"go.uber.org/zap"
 	"net"
 	"strconv"
 	"strings"
 	"time"
-
-	"go.uber.org/zap"
 
 	"github.com/gin-gonic/gin"
 	pingttl "github.com/strideynet/go-ping-ttl"
@@ -92,18 +91,16 @@ func traceroute(g *gin.RouterGroup, logger *zap.Logger, pinger pinger) {
 		}
 
 		// Set the default timeout.
-		if p.Timeout == 0 || p.Timeout > 1000 {
-			p.Timeout = 1000
+		if p.Timeout == 0 || p.Timeout > 10000 {
+			p.Timeout = 10000
 		}
 
 		// Go through each hop.
 		strResponses := []string{}
 		jsonResponses := []*TraceItem{}
 		for _, hop := range hops {
+			// Defines if the destination was reached.
 			destinationReached := false
-			// Create the context for this specific hop.
-			ctx, cancel := context.WithTimeout(c, time.Millisecond*time.Duration(p.Timeout))
-			defer cancel()
 
 			// Set the IP address and RDNS for this hop.
 			var hopIp net.Addr
@@ -123,7 +120,9 @@ func traceroute(g *gin.RouterGroup, logger *zap.Logger, pinger pinger) {
 
 			// Do our 3 tries.
 			for try := 0; try < 3; try++ {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Duration(p.Timeout)*time.Millisecond)
 				resp, err := pinger.Ping(ctx, addr, int(hop))
+				cancel()
 				if err == nil {
 					// Set the value based on the response.
 					setHopIpInfo(addr)
